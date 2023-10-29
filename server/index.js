c8bf4cd15c09f8b7725adb0dd827a59b86df404e
Sathish reddy <104757035+sathish-nalladevagari@users.noreply.github.com>
2023-10-29 10:39:56 +0530

    new storage

--- server/index.js
+++ server/index.js
@@ -20,14 +20,7 @@ app.get('*', (req, res) => {
 // Serve uploaded files statically
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'server/uploads'); // Specify the upload directory
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // Start the server
